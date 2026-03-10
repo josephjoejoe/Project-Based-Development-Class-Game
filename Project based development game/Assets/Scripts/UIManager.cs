ab1c158e6f765c6c73615c61e692c6e5870407b2
Joseph <jpraposo2027@gmail.com>
2026-03-09 21:45:43 -0400

    tools are now in the tool bar

--- Project based development game/Assets/Scripts/UIManager.cs
+++ Project based development game/Assets/Scripts/UIManager.cs
@@ -11,7 +11,7 @@ public class UIManager : MonoBehaviour
 
     }
 
-    public void ToogleToolBar()
+    public void ToggleToolBar()
     {
         toolBar.SetActive(!toolBar.activeSelf); // if tool bar is off turn on and if tool bar is on turn off
     }
