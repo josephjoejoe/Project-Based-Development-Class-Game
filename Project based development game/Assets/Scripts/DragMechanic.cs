7bbf8529d3a388d35b3e9463c7a0b77c9bcc1401
Joseph <jpraposo2027@gmail.com>
2026-03-10 12:44:56 -0400

    added to drag mechanic script. tool goes back to the tool bar when released(not dragging anymore)

--- Project based development game/Assets/Scripts/DragMechanic.cs
+++ Project based development game/Assets/Scripts/DragMechanic.cs
@@ -4,27 +4,36 @@ public class DragMechanic : MonoBehaviour
 {
     bool dragging = false;
     public Vector3 offset;
-    public GameObject toolbar;
-    
+    public GameObject toolBar;
+    public Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position; //Store the position the tool is at
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(dragging == true)
         {   //Move object, taking into account original offest
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
-            toolbar.SetActive(false);
+            toolBar.SetActive(false);
         }
     }
 
     private void OnMouseDown()
-    {   //Record the difference between the objects centre, and the clicked point on the camera plane.
+    {   //Record the difference between the objects centre, and the clicked point on the camera plane
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         dragging = true;
-        transform.SetParent(null);
+        transform.SetParent(null); //makes the tool its own parent  
     }
 
     private void OnMouseUp()
     {
-        dragging = false; // stop dragging
+        dragging = false; //stop dragging
+
+        transform.position = startPosition; //brings the tool back to its original spot
+        transform.SetParent(toolBar.transform); //makes the tool go back to the toolbar
     }
 }
