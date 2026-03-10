e202c39bfc5a603a5986267f24c41ab218f0a154
Joseph <jpraposo2027@gmail.com>
2026-03-09 21:17:16 -0400

    Made UI Manager Script. Player can now click on the Tool button on or off to see tools bar. NOTE there arent any tool yet they do not show yet

---
+++ Project based development game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject toolBar;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToogleToolBar()
+    {
+        toolBar.SetActive(!toolBar.activeSelf); // if tool bar is off turn on and if tool bar is on turn off
+    }
+
+    
+}
