370eb45dea5e4ea2a13109e2885b88c6f3b9de43
EReyes13 <itsmeelio13@gmail.com>
2026-03-11 14:42:29 -0400

    case notes
    the notes document was updated, teaching you more about case functions

--- Project based development game/Assets/Scripts/notes.cs
+++ Project based development game/Assets/Scripts/notes.cs
@@ -1,1 +1,17 @@
-//this is to offer notes or how to do certain mechanics you may find useful
+/*this is to offer notes or how to do certain mechanics you may find useful
+
+case functions serve as glorified if statements
+here is an example of a switch function
+
+switch(the variable you want deciding what goes on as a result)
+
+case variable possibility one:
+    whatever you want to happen when this variable is selected when you ran this function
+break; 
+writing break closes this case possibility and prepares for the next possibility
+case variable possibility two:
+    whatever you want to happen if the other variable is selected;
+    break;
+This case can go as long as you would like. It all just depends on how many possibilities you plan on having.
+If you want to add a new possibility, type case and then the variable responsible for that possibility,
+Then write whatever goes on in that possibility, then end with break;*/
