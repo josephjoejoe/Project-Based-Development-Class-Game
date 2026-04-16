dd151eee26b03fcd8dbf14a64d808d594fc17bfb
Joseph <jpraposo2027@gmail.com>
2026-04-16 19:41:51 -0400

    made a win condition in game manager script.

---
+++ Project based development game/Assets/Scripts/Tutorial Scripts/Appliance Scripts/ExitVentCheck.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ExitVentCheck : MonoBehaviour
+{
+    public bool isConnected;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checks"))
+        {
+            isConnected = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checks"))
+        {
+            isConnected = false;
+        }
+    }
+
+}
---
+++ Project based development game/Assets/Scripts/Tutorial Scripts/Game Manager Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public ExitVentCheck exitVC;
+    public StoveCheck stoveCheck;
+
+    //public GameObject pipes;
+    //public List<List<GameObject>> pipe = new List<List<GameObject>>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exitVC.isConnected == true && stoveCheck.isConnected == true)
+        {
+            Debug.Log("I WIN!!");
+        }
+    }
+}
